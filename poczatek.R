bbfbddbbc04bea19906ce6ad6500d89ad204f369
Oliwia Anita Podufalska <s202279@student.pg.edu.pl>
2024-12-05 17:43:09 +0100

    dodalam NA w braki

--- poczatek.R
+++ poczatek.R
@@ -15,3 +15,13 @@ na_counts
 #unikatowe wartosci
 unique_value <- data.frame(liczba_unikatowych_wartosci = sapply(dane, function(x) length(unique(na.omit(x)))))
 unique_value
+names(dane)
+length(unique(dane$ID))
+table(dane$`Marital Status`)
+table_with_na <- function(column) {
+  tab <- table(column, useNA = "ifany")  # Uwzględnienie braków danych
+  names(tab)[is.na(names(tab))] <- "NA"  # Zamiana NA na tekst "NA"
+  return(tab)
+}
+table_with_na(dane$`Marital Status`)
+
